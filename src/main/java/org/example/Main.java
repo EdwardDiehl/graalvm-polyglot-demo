b25e1a405aa4ab0110b059bbc6a2fed34caffedb
Edward Diehl <EdwDiehl@gmail.com>
2023-02-12 20:58:07 +0200

    Add code example

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Set;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+class Main {
+    public static void main(String[] args) {
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+
+        Value array = polyglot.eval("js", "(%s)(10, 50)".formatted(embeddedJavaScript));
+        System.out.println("(JS) Array: %s".formatted(array));
+
+        Value doubledArray = polyglot.eval("ruby", "%s.call([%s])".formatted(embeddedRuby, array.as(Set.class)));
+        System.out.println("(Ruby) Doubled array: %s".formatted(doubledArray));
+
+        int fifthElement = doubledArray.getArrayElement(4).asInt();
+        System.out.println("Fifth element of the doubled array: %s".formatted(fifthElement));
+    }
+
+    private static String embeddedJavaScript =
+            """
+                    (length, max) =>
+                      [...new Array(length)].map(() => Math.round(Math.random() * max))
+                    """;
+
+    private static String embeddedRuby =
+            """
+                    -> (numbers) { numbers.map { |number| number * 2 } }
+                    """;
+}
